75a208c110cc1a29fbba72490b7cd62488c58218
Melos Simeneh Mulu <85923940+melos-simeneh@users.noreply.github.com>
2023-08-30 14:17:16 -0700

    create todos using todos endpoint

--- server/index.js
+++ server/index.js
@@ -1,10 +1,34 @@
 const express = require("express");
 const cors = require("cors");
+const pool = require("./config/db");
 
 const app = express();
 
+//middleware
 app.use(cors());
 app.use(express.json());
+
+//routes
+app.post("/todos", async (req, res) => {
+  try {
+    const { description } = req.body;
+    const sql = "INSERT INTO todo (description) VALUES($1) RETURNING *";
+    const newTodo = await pool.query(sql, [description]);
+    res.status(201).json({
+      status: "success",
+      message: "Todo created",
+      data: { todo: newTodo.rows[0] },
+    });
+  } catch (error) {
+    console.log(error.message);
+    res.status(500).json({
+      status: "fail",
+      message: "Failed to create Todo",
+      errorMessage: error.message,
+      error,
+    });
+  }
+});
 
 const PORT = 8000;
 app.listen(PORT, () => {
