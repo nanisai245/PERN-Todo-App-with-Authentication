d1fa53d164903e3ed0801f3b0d51c0069581e15f
Melos Simeneh Mulu <85923940+melos-simeneh@users.noreply.github.com>
2023-08-30 14:31:41 -0700

    get a todo endpoint

--- server/index.js
+++ server/index.js
@@ -23,7 +23,7 @@ app.post("/todos", async (req, res) => {
   } catch (error) {
     res.status(500).json({
       status: "error",
-      message: "Failed to create Todo",
+      message: "Failed to create todo",
       errorMessage: error.message,
       error,
     });
@@ -42,12 +42,39 @@ app.get("/todos", async (req, res) => {
   } catch (error) {
     res.status(500).json({
       status: "error",
-      message: "Failed to create Todo",
+      message: "Failed to get todos",
       errorMessage: error.message,
       error,
     });
   }
 });
+
+//get a todo
+app.get("/todos/:id", async (req, res) => {
+  try {
+    let { id } = req.params;
+    // id = parseInt(id);
+    const todo = await pool.query("SELECT * FROM todos WHERE id=$1", [id]);
+    if (todo.rowCount === 0) {
+      return res.status(404).json({
+        staus: "fail",
+        message: `Todo with id ${id} not found`,
+      });
+    }
+    res.status(200).json({
+      staus: "success",
+      data: { todo: todo.rows[0] },
+    });
+  } catch (error) {
+    res.status(500).json({
+      status: "error",
+      message: "Failed to get a todo",
+      errorMessage: error.message,
+      error,
+    });
+  }
+});
+
 const PORT = 8000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
