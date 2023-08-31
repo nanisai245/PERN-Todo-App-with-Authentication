46b9793d38b2dab24b5f96171ad3719bd5bea965
Melos <85923940+melos-simeneh@users.noreply.github.com>
2023-08-31 15:17:19 -0700

    modal component

--- client/src/components/Modal.js
+++ client/src/components/Modal.js
@@ -1,7 +1,73 @@
-import React from "react";
+import React, { useState } from "react";
 
-const Modal = () => {
-  return <div>Modal</div>;
+const Modal = ({ mode, setShowModal, getData, task }) => {
+  const editMode = mode === "edit" ? true : false;
+  const [data, setData] = useState({
+    user_id: editMode ? task.user_id : "1",
+    title: editMode ? task.title : null,
+    progress: editMode ? task.progress : 50,
+    data: editMode ? "" : new Date(),
+  });
+
+  const postData = async (e) => {
+    e.preventDefault();
+    try {
+      console.log(data);
+      const response = await fetch("http://localhost:8000/todos", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(data),
+      });
+      if (response.status === 201) {
+        setShowModal(false);
+        getData();
+      }
+    } catch (error) {}
+  };
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setData((data) => ({ ...data, [name]: value }));
+  };
+  return (
+    <div className="overlay">
+      <div className="modal">
+        <div className="form-title-container">
+          <h3>Lets's {mode} you task</h3>
+          <button onClick={() => setShowModal(false)}>X</button>
+        </div>
+
+        <form>
+          <input
+            required
+            maxLength={30}
+            placeholder="Your task goes here"
+            name="title"
+            value={data.title}
+            onChange={handleChange}
+          />
+          <br />
+          <label htmlFor="range">Drag to select your current progress</label>
+          <input
+            required
+            type="range"
+            id="range"
+            min="0"
+            max="100"
+            name="progress"
+            value={data.progress}
+            onChange={handleChange}
+          />
+          <input
+            className={mode}
+            type="submit"
+            value="save"
+            onClick={editMode ? null : postData}
+          />
+        </form>
+      </div>
+    </div>
+  );
 };
 
 export default Modal;
