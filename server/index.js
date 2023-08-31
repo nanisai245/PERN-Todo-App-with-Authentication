1f37e44166ddd92bbd61d3ed91afe227ef021929
Melos <85923940+melos-simeneh@users.noreply.github.com>
2023-08-31 11:33:09 -0700

    get user todos endpoint

--- server/index.js
+++ server/index.js
@@ -12,9 +12,10 @@ app.use(express.json());
 //create todos
 app.post("/todos", async (req, res) => {
   try {
-    const { description } = req.body;
-    const sql = "INSERT INTO todos (description) VALUES($1) RETURNING *";
-    const newTodo = await pool.query(sql, [description]);
+    const { id, user_id, title, progress, date } = req.body;
+    const sql =
+      "INSERT INTO todos (id,user_id,title,progress,date) VALUES($1,$2,$3,$4,$5) RETURNING *";
+    const newTodo = await pool.query(sql, [id, user_id, title, progress, date]);
     res.status(201).json({
       status: "success",
       message: "Todo created",
@@ -31,7 +32,7 @@ app.post("/todos", async (req, res) => {
 });
 
 //get all todos
-app.get("/todos", async (req, res) => {
+app.get("/todos/", async (req, res) => {
   try {
     const todos = await pool.query("SELECT * FROM todos");
     res.status(200).json({
@@ -69,6 +70,29 @@ app.get("/todos/:id", async (req, res) => {
     res.status(500).json({
       status: "error",
       message: "Failed to get a todo",
+      errorMessage: error.message,
+      error,
+    });
+  }
+});
+
+//get all user todos
+app.get("/todos/users/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    console.log(id);
+    const todos = await pool.query("SELECT * FROM todos WHERE user_id=$1", [
+      id,
+    ]);
+    res.status(200).json({
+      staus: "success",
+      result: todos.rowCount,
+      data: { todos: todos.rows },
+    });
+  } catch (error) {
+    res.status(500).json({
+      status: "error",
+      message: "Failed to get todos",
       errorMessage: error.message,
       error,
     });
