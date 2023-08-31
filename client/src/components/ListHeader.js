155612d44a40f890cd9451d55caccd7924609b3c
Melos <85923940+melos-simeneh@users.noreply.github.com>
2023-08-31 15:18:32 -0700

    List header update

--- client/src/components/ListHeader.js
+++ client/src/components/ListHeader.js
@@ -1,16 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
+import Modal from "./Modal";
 
-const ListHeader = ({ listName }) => {
+const ListHeader = ({ listName, getData }) => {
+  const [showModal, setShowModal] = useState(false);
   const signOut = () => {};
   return (
     <div className="list-header">
       <h1>{listName}</h1>
       <div className="button-container">
-        <button className="create">ADD NEW</button>
+        <button className="create" onClick={() => setShowModal(true)}>
+          ADD NEW
+        </button>
         <button className="signout" onClick={signOut}>
           SIGN OUT
         </button>
       </div>
+      {showModal && (
+        <Modal mode={"create"} setShowModal={setShowModal} getData={getData} />
+      )}
     </div>
   );
 };
