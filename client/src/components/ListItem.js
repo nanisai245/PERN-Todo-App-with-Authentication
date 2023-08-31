4d3db79546b96031cf4d757a5ec8fdb1562a14a1
Melos <85923940+melos-simeneh@users.noreply.github.com>
2023-08-31 11:32:54 -0700

    list item update

--- client/src/components/ListItem.js
+++ client/src/components/ListItem.js
@@ -1,7 +1,21 @@
-import React from "react";
+import TickIcon from "./TickIcon";
+import ProgressBar from "./ProgressBar";
 
-const ListItem = () => {
-  return <div>ListItem</div>;
+const ListItem = ({ task }) => {
+  return (
+    <div className="list-item">
+      <div className="info-container">
+        <TickIcon />
+        <p className="task-title">{task.title}</p>
+        <ProgressBar />
+      </div>
+
+      <div className="button-container">
+        <button className="edit">EDIT</button>
+        <button className="delete">DELETE</button>
+      </div>
+    </div>
+  );
 };
 
 export default ListItem;
